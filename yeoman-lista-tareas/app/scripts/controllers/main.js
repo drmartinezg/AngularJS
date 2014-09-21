29185c4f1aea8c9ea05c42a6736a72fdcf8cd255
Daniel-Ramon Martinez <drmartinezg@gmail.com>
2014-09-21 22:51:56 +0200

    Añadir y borrar tareas

--- yeoman-lista-tareas/app/scripts/controllers/main.js
+++ yeoman-lista-tareas/app/scripts/controllers/main.js
@@ -9,9 +9,13 @@
  */
 angular.module('yeomanListaTareasApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    $scope.tareas = ['Item 1', 'Item 2', 'Item 3', 'Item 4'];
+
+    $scope.addTarea = function() {
+    	$scope.tareas.push($scope.tarea);
+    	$scope.tarea = '';
+    };
+    $scope.eliminarTarea = function(index) {
+    	$scope.tareas.splice(index, 1);
+    };
   });
