805baa9fdaf39a6db8c0932c85e5a43ffb92dcc5
Daniel-Ramon Martinez <drmartinezg@gmail.com>
2014-09-25 22:43:22 +0200

    Curso básico AngularJS - Ejemplos 03- Final

--- ejemplos/03-CuadernoAlumnos/scripts1.js
+++ ejemplos/03-CuadernoAlumnos/scripts1.js
@@ -10,5 +10,15 @@ function AlumnosController($scope) {
 		$scope.alumnos.push({nombre: $scope.nuevoAlumno.nombre,
 							 telefono: $scope.nuevoAlumno.telefono,
 							 curso: $scope.nuevoAlumno.curso});
+
+		$scope.formVisibility=false;
 	}
+
+	$scope.formVisibility=false;
+
+	$scope.ShowForm=function() {
+		$scope.formVisibility=true;
+		console.log($scope.formVisibility);
+	}
+
 }
