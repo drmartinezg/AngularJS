222a1716c32ca61ab6e0afd26b20ceefe638a0b4
Daniel-Ramon Martinez <drmartinezg@gmail.com>
2014-09-29 22:55:11 +0200

    Curso básico AngularJS - Ejemplos 04 - grunt 04

--- ejemplos/MiProyecto/gruntfile.js
+++ ejemplos/MiProyecto/gruntfile.js
@@ -16,12 +16,22 @@ module.exports = function (grunt) {
 				src: 'unidos.js',
 				dest: 'build/unidos.min.js'
 			}
+		},
+		shell: {
+			multiple: {
+				command: [
+					'del unidos.js',
+					'mkdir deploy',
+					'move build\\unidos.min.js deploy\\unidos.min.js'
+				].join('&&')
+			}
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-shell');
 	// Default task.
-	grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
+	grunt.registerTask('default', ['jshint', 'concat', 'uglify','shell']);
 };
