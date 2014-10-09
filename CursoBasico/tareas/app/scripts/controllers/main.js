bc853133c6ae2967bce2928a088d206c4e7a4b26
Daniel-Ramon Martinez <drmartinezg@gmail.com>
2014-10-09 23:10:41 +0200

    Curso básico AngularJS - Ejemplos 05 - tareas 03

--- CursoBasico/tareas/app/scripts/controllers/main.js
+++ CursoBasico/tareas/app/scripts/controllers/main.js
@@ -9,12 +9,7 @@
  */
 angular.module('tareasApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.tareas = [
-      'Item 1',
-      'Item 2',
-      'Item 3',
-      'Item 4'
-    ];
+    $scope.tareas = [];
 
     $scope.addTarea = function () {
     	$scope.tareas.push($scope.tarea);
