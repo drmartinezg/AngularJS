0a568c0f15703f9e54ceaa6391a2ce3e4358447c
Daniel-Ramon Martinez <drmartinezg@gmail.com>
2014-11-09 20:57:30 +0100

    Cuso AngularJs - 04_05_ngoptions

---
+++ CursoAngularJs/04_05_ngoptions/script.js
@@ -0,0 +1,32 @@
+var app = angular.module("app", []);
+ 
+app.controller("PruebaController", function($scope) {
+  $scope.provincias=[
+    {
+      idProvincia:2,
+      nombre:"Castellon"
+    },
+    {
+      idProvincia:3,
+      nombre:"Alicante"
+    },
+    {
+      idProvincia:1,
+      nombre:"Valencia"
+    },
+    {
+      idProvincia:7,
+      nombre:"Teruel"
+    },  
+    {
+      idProvincia:5,
+      nombre:"Tarragona"
+    }
+  ];
+    
+  $scope.miProvinciaSeleccionada={
+      idProvincia:3,
+      nombre:"Alicante"
+    }
+
+});
