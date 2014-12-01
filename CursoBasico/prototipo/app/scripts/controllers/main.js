36922953fe5214535ae8b818b02df0223672cf83
Daniel-Ramon Martinez <drmartinezg@gmail.com>
2014-12-01 08:35:07 +0100

    Curso Basico AngularJS - Creación de prototipos con Yeoman (22-b)

--- CursoBasico/prototipo/app/scripts/controllers/main.js
+++ CursoBasico/prototipo/app/scripts/controllers/main.js
@@ -9,9 +9,24 @@
  */
 angular.module('prototipoApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  	var baseURL = 'http://lorempixel.com/960/450/';
+  	$scope.setInterval = 5000;
+
+  	$scope.slides = [
+		{
+			title: 'Aprender a mantenerte en forma',
+			image: baseURL + 'sports/',
+			text: '¡Practica algún deporte todos los días'
+		},
+		{
+			title: 'Buena alimentación',
+			image: baseURL + 'food/',
+			text: '¡La importacia de frutas y verduras en la dieta!'
+		},
+		{
+			title: 'En contacto con la naturaleza',
+			image: baseURL + 'nature/',
+			text: '¡Mantente en armonía con la naturaleza!'
+		}
+  	];
   });
