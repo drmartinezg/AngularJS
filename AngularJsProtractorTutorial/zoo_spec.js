22a2b24ccd737453d19a104f6461eb3a10fb4145
Daniel-Ramon Martinez <drmartinezg@gmail.com>
2015-07-08 14:20:13 +0200

    Angular-Protractor -05- Before and After

--- AngularJsProtractorTutorial/zoo_spec.js
+++ AngularJsProtractorTutorial/zoo_spec.js
@@ -1,34 +1,52 @@
 describe('Adopt an animal on the zoo test site', function() {
 
-	it('should able on correct page', function() {
+	beforeEach(function() {
 		browser.get("http://www.thetestroom.com/jswebapp/");
-		expect(browser.getCurrentUrl()).toContain("jswebapp");
+	});
+
+	afterEach(function() {
+		console.log("After method executed");
+	});
+
+	it('should able on correct page', function() {
 		expect(browser.getCurrentUrl()).toEqual("http://www.thetestroom.com/jswebapp/");
 	});	
 
-	it('should generate correct text', function() {
-		browser.get("http://www.thetestroom.com/jswebapp/");
-		var textMessage = "Manuel es el nombre de la persona";
+	describe('should generate correct text', function() {
+		var textMessage;
 
-		element(by.model("person.name")).sendKeys(textMessage);
-		element(by.binding("person.name")).getText().then(function(text) {
-			expect(text).toEqual(textMessage);
+		beforeEach(function() {
+			textMessage = "Manuel es el nombre de la persona";			
+		});
+
+		it('should check correct text', function() {
+			element(by.model("person.name")).sendKeys(textMessage);
+			element(by.binding("person.name")).getText().then(function(text) {
+				expect(text).toEqual(textMessage);
+			});
 		});
 	});	
 
-	it('should check the correct number of items in drop down', function() {
-		browser.get("http://www.thetestroom.com/jswebapp/");
-		element(by.buttonText("CONTINUE")).click();
-		element(by.model("animal")).$('[value="1"]').click();
+	describe('should check the correct number of items in drop down', function() {
 
-		element.all(by.css(".ng-pristine option")).then(function(items) {
-			expect(items.length).toBe(4);
-			expect(items[1].getText()).toBe("George the Turtle");
+		var lengthOfItems;
+
+		beforeEach(function() {
+			lengthOfItems = 4;
+		});
+
+		it('should check number of items', function() {
+			element(by.buttonText("CONTINUE")).click();
+			element(by.model("animal")).$('[value="1"]').click();
+
+			element.all(by.css(".ng-pristine option")).then(function(items) {
+				expect(items.length).toBe(lengthOfItems);
+				expect(items[1].getText()).toBe("George the Turtle");
+			});
 		});
 	});	
 
 	it('should check user is on the thank you page', function() {
-		browser.get("http://www.thetestroom.com/jswebapp/");
 		element(by.buttonText("CONTINUE")).click();
 		element(by.buttonText("CONTINUE")).click();
 		expect(browser.getCurrentUrl()).toContain("confirm");
