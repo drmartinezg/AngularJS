cf6c19d5f2393e67be79fa391e1321d263271fae
Daniel-Ramon Martinez <drmartinezg@gmail.com>
2015-07-08 11:25:17 +0200

    Angular-Protractor -04- Using Expects

--- AngularJsProtractorTutorial/zoo_spec.js
+++ AngularJsProtractorTutorial/zoo_spec.js
@@ -1,15 +1,37 @@
 describe('Adopt an animal on the zoo test site', function() {
 
-	it('should able to adopt an animal', function() {
-		browser.get("http://www.thetestroom.com/jswebapp/")
-		element(by.model("person.name")).sendKeys("Manuel");
+	it('should able on correct page', function() {
+		browser.get("http://www.thetestroom.com/jswebapp/");
+		expect(browser.getCurrentUrl()).toContain("jswebapp");
+		expect(browser.getCurrentUrl()).toEqual("http://www.thetestroom.com/jswebapp/");
+	});	
+
+	it('should generate correct text', function() {
+		browser.get("http://www.thetestroom.com/jswebapp/");
+		var textMessage = "Manuel es el nombre de la persona";
+
+		element(by.model("person.name")).sendKeys(textMessage);
 		element(by.binding("person.name")).getText().then(function(text) {
-			console.log(text);
+			expect(text).toEqual(textMessage);
 		});
+	});	
+
+	it('should check the correct number of items in drop down', function() {
+		browser.get("http://www.thetestroom.com/jswebapp/");
 		element(by.buttonText("CONTINUE")).click();
 		element(by.model("animal")).$('[value="1"]').click();
-		element(by.partialButtonText("CONT")).click();
 
+		element.all(by.css(".ng-pristine option")).then(function(items) {
+			expect(items.length).toBe(4);
+			expect(items[1].getText()).toBe("George the Turtle");
+		});
+	});	
+
+	it('should check user is on the thank you page', function() {
+		browser.get("http://www.thetestroom.com/jswebapp/");
+		element(by.buttonText("CONTINUE")).click();
+		element(by.buttonText("CONTINUE")).click();
+		expect(browser.getCurrentUrl()).toContain("confirm");
 	});	
 
 });
